739053c281172163cddf7db0278a5cb6d6ad4e51
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-20 19:33:54 +0000

    make all

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 """
-Main cli or app entry point
+main code
+
 """
 
-from mylib.calculator import add
-import click
-
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+import pandas as pd
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def csvfile(main_data):
+    csv = pd.read_csv(main_data)
+    return csv
+
+
+# read the CSV file
+df = csvfile("titanic_main_data.csv")
+
+# compute summary statistics
+summary = df.describe()
+
+# plot a histogram of the "Age" column
+plt.hist(df["Age"], bins=10)
+plt.xlabel("Age")
+plt.ylabel("Frequency")
+plt.title("Histogram of Age")
+plt.show()
+
+
+# generate a analyse report
+def report_csvfile():
+    csv2 = pd.read_csv("titanic_main_data.csv")
+    csv_report = csv2.loc[0:301, ["Age", "Pclass"]]
+    profile = ProfileReport(
+        csv_report, title="Titanic People Age Report", explorative=True
+    )
+    profile.to_file("data_profiling_report.html")
--- test_main.py
+++ test_main.py
@@ -2,9 +2,29 @@
 Test goes here
 
 """
+from main import csvfile
 
-from mylib.calculator import add
+
+def test_csvfile():
+    main_data = "titanic_main_data.csv"
+    result = csvfile(main_data)
+
+    # mean value of the 1st column
+    mean_1_column = result.iloc[:, 0].mean()
+    # print(mean_1_column)
+    assert mean_1_column == 446
+    # min value of 1st column
+    min_1_column = result.iloc[:, 0].min()
+    # print(min_1_column)
+    assert min_1_column == 1
 
 
-def test_add():
-    assert add(1, 2) == 3
+if __name__ == "__main__":
+    test_csvfile()
+
+
+# from mylib.calculator import add
+
+
+# def test_add():
+#     assert add(1, 2) == 3
